bf34a29a87814c698411e5e827b5c17c23c616fe
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-08 18:49:49 -0800

    first cut login page
    Checking in Login page

--- gen/com/example/fantasyfinance/R.java
+++ gen/com/example/fantasyfinance/R.java
@@ -21,21 +21,42 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int finance=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int Button01=0x7f080009;
+        public static final int TextView01=0x7f080004;
+        public static final int action_settings=0x7f08000a;
+        public static final int button1=0x7f080006;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080005;
+        public static final int imageButton1=0x7f080008;
+        public static final int imageView1=0x7f080001;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int textView3=0x7f080007;
     }
     public static final class layout {
         public static final int activity_home=0x7f030000;
     }
     public static final class menu {
         public static final int home=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int attempts=0x7f050006;
+        public static final int cancel=0x7f05000b;
+        public static final int finance=0x7f050003;
+        public static final int forgot=0x7f05000a;
         public static final int hello_world=0x7f050002;
+        public static final int login=0x7f050007;
+        public static final int password=0x7f050005;
+        public static final int register=0x7f050008;
+        public static final int title_activity_main=0x7f050009;
+        public static final int username=0x7f050004;
     }
     public static final class style {
         /** 
