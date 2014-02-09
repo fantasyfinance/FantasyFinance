63a6043ec68db30349d02f3e9bd727ce7d454b3e
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-08 19:00:43 -0800

    generated events in login screen
    generated methods for all the buttons on login screen

--- src/com/example/fantasyfinance/Home.java
+++ src/com/example/fantasyfinance/Home.java
@@ -1,23 +1,47 @@
 package com.example.fantasyfinance;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 public class Home extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home);
+	}
 
+	public void onLoginClick(View v) {
+		Toast.makeText(getApplicationContext(), "Clicked Login !!!", 
+				   Toast.LENGTH_LONG).show();
+		
+	}
+	
+	public void onRegisterClick(View v) {
+		Toast.makeText(getApplicationContext(), "Clicked Register !!!", 
+				   Toast.LENGTH_LONG).show();
+	}
+	
+	public void onForgotPassword(View v) {
+		Toast.makeText(getApplicationContext(), "Clicked Forgot !!!", 
+				   Toast.LENGTH_LONG).show();
+		
+	}
+	
+	public void onResetClick(View v) {
+		Toast.makeText(getApplicationContext(), "Clicked Reset !!!", 
+				   Toast.LENGTH_LONG).show();
+		
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
-        return true;
-    }
-    
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.home, menu);
+		return true;
+	}
+
 }
