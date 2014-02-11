8ab796985755c72000d548ad4ae7a219dfe66dc1
Pavan Tej Medapuram <pavan.teju@yahoo.com>
2014-02-10 20:51:29 -0800

    Added Delete Bin

--- gen/com/example/fantasyfinance/R.java
+++ gen/com/example/fantasyfinance/R.java
@@ -22,9 +22,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int finance=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int ic_logout=0x7f020002;
-        public static final int ic_tagline=0x7f020003;
+        public static final int ic_delete_bin=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int ic_logout=0x7f020003;
+        public static final int ic_tagline=0x7f020004;
     }
     public static final class id {
         public static final int Button01=0x7f080008;
