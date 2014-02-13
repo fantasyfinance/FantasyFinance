601e2647c7dddcd9a1638f5887cb933a8cbe6f70
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-12 20:03:25 -0800

    code for updating tagline
    checking in the code which updates the tagline of a given user.

--- gen/com/example/fantasyfinance/R.java
+++ gen/com/example/fantasyfinance/R.java
@@ -36,11 +36,10 @@ public final class R {
         public static final int editText2=0x7f080002;
         public static final int imageButton1=0x7f080001;
         public static final int imageView1=0x7f080005;
-        public static final int logout=0x7f080011;
-        public static final int searchBox=0x7f08000e;
-        public static final int tagline=0x7f080010;
+        public static final int logout=0x7f080010;
+        public static final int tagText=0x7f08000e;
+        public static final int tagline=0x7f08000d;
         public static final int textView3=0x7f080000;
-        public static final int uname=0x7f08000d;
         public static final int usergroups=0x7f08000b;
         public static final int userivProfileImage=0x7f080007;
         public static final int userllCounts=0x7f08000a;
@@ -64,18 +63,19 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int attempts=0x7f050006;
-        public static final int cancel=0x7f05000b;
+        public static final int cancel=0x7f05000c;
         public static final int finance=0x7f050003;
-        public static final int forgot=0x7f05000a;
+        public static final int forgot=0x7f05000b;
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050007;
-        public static final int logout=0x7f05000d;
+        public static final int logout=0x7f05000e;
         public static final int password=0x7f050005;
-        public static final int register=0x7f050008;
-        public static final int tagline=0x7f05000e;
-        public static final int title_activity_login=0x7f05000c;
-        public static final int title_activity_main=0x7f050009;
-        public static final int title_activity_tagline=0x7f05000f;
+        public static final int register=0x7f050009;
+        public static final int tagline=0x7f05000f;
+        public static final int title_activity_login=0x7f05000d;
+        public static final int title_activity_main=0x7f05000a;
+        public static final int title_activity_tagline=0x7f050010;
+        public static final int update=0x7f050008;
         public static final int username=0x7f050004;
     }
     public static final class style {
--- src/com/example/fantasyfinance/Tagline.java
+++ src/com/example/fantasyfinance/Tagline.java
@@ -1,31 +1,45 @@
 package com.example.fantasyfinance;
 
-import com.example.utils.Constants;
-import com.parse.ParseUser;
+import java.util.List;
 
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.utils.Constants;
+import com.parse.FindCallback;
+import com.parse.GetCallback;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class Tagline extends Activity {
 
-	ParseUser currentUser ;
-	
+	ParseUser currentUser;
+	EditText et;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tagline);
-		currentUser = ParseUser.getCurrentUser(); 
-		if (currentUser!=null)
-		{
-			TextView tvLabel = (TextView) findViewById(R.id.uname);
-			tvLabel.setText(getIntent().getStringExtra("uname") + "'s "+Constants.yourTag);
-			
-			
+		ActionBar actionBar = getActionBar();
+		actionBar.setBackgroundDrawable(new ColorDrawable(Color
+				.parseColor("#000000")));
+		currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			TextView tvLabel = (TextView) findViewById(R.id.tagline);
+			tvLabel.setText(getIntent().getStringExtra("uname") + "'s "
+					+ Constants.yourTag);
 		}
 	}
 
@@ -36,4 +50,39 @@ public class Tagline extends Activity {
 		return true;
 	}
 
+	public void onTaglineClick(View v) {
+		currentUser = ParseUser.getCurrentUser();
+		if (currentUser != null) {
+			// Add your initialization code here
+			Parse.initialize(this, Constants.APPLICATION_KEY,
+					Constants.APPLICATION_KEY_TOKEN);
+
+			ParseQuery<ParseObject> query = ParseQuery.getQuery("TaglineModel");
+			query.whereEqualTo("user", currentUser.getUsername());
+			query.findInBackground(new FindCallback<ParseObject>() {
+				public void done(List<ParseObject> tags, ParseException e) {
+					if (tags.size() > 0 && e == null) {
+						String objectID = tags.get(0).getObjectId();
+						ParseQuery<ParseObject> query = ParseQuery
+								.getQuery("TaglineModel");
+						query.getInBackground(objectID,
+								new GetCallback<ParseObject>() {
+									public void done(ParseObject gameScore,
+											ParseException e) {
+										if (e == null) {
+											EditText et = (EditText) findViewById(R.id.tagText);
+											String tag = et.getText().toString();
+											gameScore.put("user",currentUser.getUsername());
+											gameScore.put("tag", tag);
+											gameScore.saveInBackground();
+										}
+									}
+								});
+					} else {
+						Log.d("DEBUG", "Error: " + e.getMessage());
+					}
+				}
+			});
+		}
+	}
 }
