2e6488d5b45217827208dc6f5e000c51c359a4f5
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-13 19:54:08 -0800

    Registration form First Cut
    Registration form first cut with all the validations

--- gen/com/example/fantasyfinance/R.java
+++ gen/com/example/fantasyfinance/R.java
@@ -31,15 +31,18 @@ public final class R {
         public static final int textinputborder=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000f;
+        public static final int action_settings=0x7f080012;
         public static final int button1=0x7f080004;
         public static final int editText1=0x7f080003;
         public static final int editText2=0x7f080002;
+        public static final int emailID=0x7f08000f;
         public static final int imageButton1=0x7f080001;
         public static final int imageView1=0x7f080005;
-        public static final int logout=0x7f080010;
-        public static final int tagText=0x7f08000e;
-        public static final int tagline=0x7f08000d;
+        public static final int logout=0x7f080013;
+        public static final int registerUsername=0x7f08000e;
+        public static final int registerpwd=0x7f08000d;
+        public static final int tagText=0x7f080011;
+        public static final int tagline=0x7f080010;
         public static final int textView3=0x7f080000;
         public static final int usergroups=0x7f08000b;
         public static final int userivProfileImage=0x7f080007;
@@ -67,6 +70,7 @@ public final class R {
         public static final int app_name=0x7f050000;
         public static final int attempts=0x7f050006;
         public static final int cancel=0x7f05000c;
+        public static final int email=0x7f050012;
         public static final int finance=0x7f050003;
         public static final int forgot=0x7f05000b;
         public static final int hello_world=0x7f050002;
--- src/com/example/fantasyfinance/Register.java
+++ src/com/example/fantasyfinance/Register.java
@@ -2,18 +2,28 @@ package com.example.fantasyfinance;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
+import com.example.utils.Constants;
+import com.parse.Parse;
 import com.parse.ParseUser;
 
 public class Register extends Activity {
 
 	ParseUser currentUser;
-	Context context;
+	final Context context = this;
+	EditText usernameField;
+	EditText passwordField;
+	EditText emailField;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +31,79 @@ public class Register extends Activity {
 		ActionBar actionBar = getActionBar();
 		actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
 		
+	}
+	
+	public void onRegisterClick(View v) {
+		//Toast.makeText(getApplicationContext(), "Clicked Register !!!",Toast.LENGTH_LONG).show();
+		usernameField = (EditText)findViewById(R.id.registerUsername);
+		String username = usernameField.getText().toString();
+		passwordField = (EditText)findViewById(R.id.registerpwd);
+		String pwd = usernameField.getText().toString();
+		emailField = (EditText)findViewById(R.id.emailID);
+		String email = emailField.getText().toString();
+		Parse.initialize(this, Constants.APPLICATION_KEY,
+				Constants.APPLICATION_KEY_TOKEN);
+		currentUser = ParseUser.getCurrentUser();
+		if(currentUser == null)
+		{
+			boolean clientValidation = validateAllFields(username,pwd,email);
+			
+			//code which says to enter correct credentials
+			if(!clientValidation)
+			{
+				AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+				alertDialogBuilder.setTitle("Incorrect Crendentials");
+				alertDialogBuilder.setMessage("Please Enter Correct Credentials").setCancelable(false)
+					.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog,int id) {
+							Intent logout = new Intent(getApplicationContext(), Home.class);
+							startActivity(logout);
+							
+						}
+					  });
+				AlertDialog alertDialog = alertDialogBuilder.create();
+				alertDialog.show();
+			}
+			
+			//code which creates correct user
+			else
+			{
+			}
+			
+			
+		}
+		
+		//code which makes the user logout from the application inorder to register
+		else
+		{
+			AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+			alertDialogBuilder.setTitle("Logging out to create an account");
+			alertDialogBuilder.setMessage("Click Ok to exit!").setCancelable(false)
+				.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog,int id) {
+						ParseUser.logOut();
+						currentUser = ParseUser.getCurrentUser();
+						invalidateOptionsMenu();
+						Intent logout = new Intent(getApplicationContext(), Home.class);
+						startActivity(logout);
+						
+					}
+				  });
+			AlertDialog alertDialog = alertDialogBuilder.create();
+			alertDialog.show();
+		}
+	}
+	private boolean validateAllFields(String uname,String pwd,String email) {
+		// TODO Auto-generated method stub
+		if (uname.matches("")|| pwd.matches("")||email.matches(""))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+			
 	}
 
 	@Override
