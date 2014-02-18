9c082136cb25408ec563bd3afe74dfc2bfced5f9
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-17 21:27:04 -0800

    search utility for all companies
    Search module which has all the companies and will search based on the
    text entered.

--- gen/com/example/fantasyfinance/R.java
+++ gen/com/example/fantasyfinance/R.java
@@ -31,7 +31,7 @@ public final class R {
         public static final int textinputborder=0x7f020007;
     }
     public static final class id {
-        public static final int action_settings=0x7f080017;
+        public static final int action_settings=0x7f080019;
         public static final int button1=0x7f080001;
         public static final int editText1=0x7f080006;
         public static final int editText2=0x7f080005;
@@ -40,14 +40,16 @@ public final class R {
         public static final int forgotemailID=0x7f080002;
         public static final int imageButton1=0x7f080004;
         public static final int imageView1=0x7f080007;
-        public static final int logout=0x7f080018;
+        public static final int listView1=0x7f080015;
+        public static final int logout=0x7f08001a;
         public static final int pager=0x7f080008;
         public static final int predict_username=0x7f080009;
+        public static final int product_name=0x7f080018;
         public static final int registerUsername=0x7f080012;
         public static final int registerpwd=0x7f080011;
-        public static final int search_username=0x7f080014;
-        public static final int tagText=0x7f080016;
-        public static final int tagline=0x7f080015;
+        public static final int searchStock=0x7f080014;
+        public static final int tagText=0x7f080017;
+        public static final int tagline=0x7f080016;
         public static final int textView3=0x7f080003;
         public static final int usergroups=0x7f08000f;
         public static final int userivProfileImage=0x7f08000b;
@@ -67,6 +69,7 @@ public final class R {
         public static final int activity_register=0x7f030006;
         public static final int activity_search_stocks=0x7f030007;
         public static final int activity_tagline=0x7f030008;
+        public static final int list_item=0x7f030009;
     }
     public static final class menu {
         public static final int followed_stocks=0x7f070000;
@@ -84,27 +87,28 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int attempts=0x7f050007;
-        public static final int cancel=0x7f05000d;
-        public static final int email=0x7f050013;
+        public static final int cancel=0x7f05000e;
+        public static final int companyName=0x7f050009;
+        public static final int email=0x7f050014;
         public static final int finance=0x7f050003;
-        public static final int forgot=0x7f05000c;
+        public static final int forgot=0x7f05000d;
         public static final int hello_world=0x7f050002;
         public static final int login=0x7f050008;
-        public static final int logout=0x7f05000f;
+        public static final int logout=0x7f050010;
         public static final int password=0x7f050006;
-        public static final int register=0x7f05000a;
+        public static final int register=0x7f05000b;
         public static final int submit=0x7f050004;
-        public static final int tagline=0x7f050010;
-        public static final int title_activity_followed_stocks=0x7f050016;
-        public static final int title_activity_forgot_password=0x7f050014;
-        public static final int title_activity_login=0x7f05000e;
-        public static final int title_activity_main=0x7f05000b;
-        public static final int title_activity_predict_stocks=0x7f050018;
-        public static final int title_activity_profile=0x7f050015;
-        public static final int title_activity_register=0x7f050012;
-        public static final int title_activity_search_stocks=0x7f050017;
-        public static final int title_activity_tagline=0x7f050011;
-        public static final int update=0x7f050009;
+        public static final int tagline=0x7f050011;
+        public static final int title_activity_followed_stocks=0x7f050017;
+        public static final int title_activity_forgot_password=0x7f050015;
+        public static final int title_activity_login=0x7f05000f;
+        public static final int title_activity_main=0x7f05000c;
+        public static final int title_activity_predict_stocks=0x7f050019;
+        public static final int title_activity_profile=0x7f050016;
+        public static final int title_activity_register=0x7f050013;
+        public static final int title_activity_search_stocks=0x7f050018;
+        public static final int title_activity_tagline=0x7f050012;
+        public static final int update=0x7f05000a;
         public static final int username=0x7f050005;
     }
     public static final class style {
--- src/com/example/fantasyfinance/SearchStocks.java
+++ src/com/example/fantasyfinance/SearchStocks.java
@@ -1,38 +1,110 @@
 package com.example.fantasyfinance;
 
-
+import java.util.ArrayList;
+import java.util.List;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.utils.Constants;
 import com.parse.ParseUser;
 
 public class SearchStocks extends Fragment {
-	
+
 	String username;
 	ParseUser currentUser;
+	// List view
+	public ListView lv;
+
+	// Listview Adapter
+	ArrayAdapter<String> adapter;
+
+	// Search EditText
+	EditText inputSearch;
+
 	final Context context = getActivity();
+	List<String> array_sort = new ArrayList<String>();
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
- 
-        View rootView = inflater.inflate(R.layout.activity_search_stocks, container, false);
-        currentUser = ParseUser.getCurrentUser();
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.activity_search_stocks,
+				container, false);
+		currentUser = ParseUser.getCurrentUser();
+		
 		if (currentUser != null) {
 			Bundle b = getActivity().getIntent().getExtras();
 			username = b.getString("username");
-			TextView tvLabel = (TextView) rootView.findViewById(R.id.search_username);
-			tvLabel.setText(username);
 		} else {
 			Intent intent = new Intent(getActivity(), Home.class);
 			startActivity(intent);
-			
 		}
-        return rootView;
-    }
+		return rootView;
+	}
+
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		lv = (ListView) getActivity().findViewById(R.id.listView1);
+		lv.setAdapter(new ArrayAdapter<String>(getActivity(),
+				android.R.layout.simple_list_item_1, Constants.companies));
+		
+		lv.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+
+				// selected item
+				String selectedFromList = (String) (lv.getItemAtPosition(position));
+				Toast.makeText(getActivity().getApplicationContext(),selectedFromList, Toast.LENGTH_LONG).show();
+			}
+		});
+		
+		inputSearch = (EditText) getActivity().findViewById(R.id.searchStock);
+		inputSearch.addTextChangedListener(new TextWatcher() {
+
+			@Override
+			public void onTextChanged(CharSequence cs, int arg1, int arg2,
+					int arg3) {
+				// When user changed the Text
+				
+				array_sort.clear();
+				
+				for (int i = 0; i < Constants.companies.length; i++)
+				{
+					if(Constants.companies[i].toString().contains(inputSearch.getText().toString()))
+					{
+						array_sort.add(Constants.companies[i].toString());
+					}
+				}
+				lv.setAdapter(new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1, array_sort));
+			}
+
+			@Override
+			public void beforeTextChanged(CharSequence arg0, int arg1,
+					int arg2, int arg3) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void afterTextChanged(Editable arg0) {
+				// TODO Auto-generated method stub
+			}
+		});
+
+	}
 }
