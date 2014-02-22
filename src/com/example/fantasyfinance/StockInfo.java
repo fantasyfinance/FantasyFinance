e613ed4770e517355c47de95a80e578bbc66e72b
Dheeraj Rampally <rampallydheeraj@gmail.com>
2014-02-22 00:31:59 -0800

    logic for stock saving and prediction
    logic for stock saving and prediction

--- src/com/example/fantasyfinance/StockInfo.java
+++ src/com/example/fantasyfinance/StockInfo.java
@@ -5,6 +5,8 @@ import java.util.List;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -13,6 +15,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.utils.Constants;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
@@ -25,6 +28,8 @@ public class StockInfo extends Activity {
 	EditText et;
 	String username;
 	String stock;
+	Button p1_button;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -35,32 +40,27 @@ public class StockInfo extends Activity {
 			stock = getIntent().getStringExtra("selectedStock");
 			tvLabel.setText(stock);
 			username = getIntent().getStringExtra("username");
-			ParseQuery<ParseObject> query = ParseQuery.getQuery("UserStockPreference");
+			ParseQuery<ParseObject> query = ParseQuery
+					.getQuery("UserStockPreference");
 			query.whereEqualTo("user", username);
-			query.whereEqualTo("stock","AAPL");
-			Log.d("DEBUG",stock);
-			Log.d("DEBUG",username);
+			query.whereEqualTo("stock", stock);
+			Log.d("DEBUG", stock);
+			Log.d("DEBUG", username);
 			
-			query.findInBackground(new FindCallback<ParseObject>() 
-			{
-				public void done(List<ParseObject> records, ParseException e) 
-				{
-					if (records.size() > 0 && e == null) 
-					{
-						Log.d("DEBUG","PRESENT");
-						Button p1_button = (Button)findViewById(R.id.button1);
-						p1_button.setText("UnFollow");
-						//so write the code for submit unfollow button
-					} 
-					else if(e != null) {
+			query.findInBackground(new FindCallback<ParseObject>() {
+				public void done(List<ParseObject> records, ParseException e) {
+					if (records.size() > 0 && e == null) {
+						Log.d("DEBUG", "PRESENT");
+						p1_button = (Button) findViewById(R.id.button1);
+						p1_button.setText(Constants.unFollow);
+						// so write the code for submit unfollow button
+					} else if (e != null) {
 						Log.d("DEBUG", "Error: " + e.getMessage());
-					}
-					else if(records.size() == 0 && e == null)
-					{
-						Log.d("DEBUG","ABSENT");
-						Button p1_button = (Button)findViewById(R.id.button1);
-						p1_button.setText("Follow");
-						//so write the code for submit follow button
+					} else if (records.size() == 0 && e == null) {
+						Log.d("DEBUG", "ABSENT");
+						p1_button = (Button) findViewById(R.id.button1);
+						p1_button.setText(Constants.follow);
+						// so write the code for submit follow button
 					}
 				}
 			});
@@ -75,13 +75,70 @@ public class StockInfo extends Activity {
 		startActivity(logout);
 		StockInfo.this.finish();
 	}
-	
-	
-	public void onStatusClick(View v)
-	{
-		Log.d("DEBUG","Clicked");
+
+	public void onStatusClick(View v) {
+		
+		if (p1_button.getText().equals("Follow")) {
+			Log.d("DEBUG","Follow");
+			username = getIntent().getStringExtra("username");
+			stock = getIntent().getStringExtra("selectedStock");
+			
+			ParseObject userstock = new ParseObject("UserStockPreference");
+			userstock.put("user", username);
+			userstock.put("stock", stock);
+			userstock.saveInBackground();
+			
+			ParseObject prediction = new ParseObject("Predict");
+			prediction.put("user", username);
+			prediction.put("stock", stock);
+			prediction.put("lock", "no");
+			prediction.put("prediction", "n/a");
+			prediction.saveInBackground();
+			
+			
+			Intent intent = new Intent(this, Login.class);
+			intent.putExtra("username",username);
+			intent.putExtra("stock", stock);
+			startActivity(intent);
+			
+		} else {
+			Log.d("DEBUG","UnFollow");
+			username = getIntent().getStringExtra("username");
+			stock = getIntent().getStringExtra("selectedStock");
+			ParseQuery<ParseObject> query = ParseQuery
+					.getQuery("UserStockPreference");
+			query.whereEqualTo("user", username);
+			query.whereEqualTo("stock", stock);
+			query.findInBackground(new FindCallback<ParseObject>() {
+				public void done(List<ParseObject> records, ParseException e) {
+					if (records.size() == 1 && e == null) {
+						records.get(0).deleteInBackground();
+						ParseQuery<ParseObject> query = ParseQuery
+								.getQuery("Predict");
+						query.whereEqualTo("user", username);
+						query.whereEqualTo("stock", stock);
+						query.findInBackground(new FindCallback<ParseObject>() {
+							public void done(List<ParseObject> records, ParseException e) {
+								if (records.size() == 1 && e == null) {
+									records.get(0).deleteInBackground();
+								} else if (records.size() == 0 && e == null) {
+									
+								}
+							}
+						});
+					} else if (records.size() == 0 && e == null) {
+						
+					}
+				}
+			});
+			
+			Intent intent = new Intent(this, Login.class);
+			intent.putExtra("username",username);
+			intent.putExtra("stock", stock);
+			startActivity(intent);
+		}
 	}
-	
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
